2a7c7c716d29e18df4161fb1f07e3d7ac6f661fd
Erik Olson <111093132+ErikAmerico@users.noreply.github.com>
2023-05-02 16:48:25 -0400

    removed comments

--- script.js
+++ script.js
@@ -1,13 +1,5 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
+
 $(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
 
   const saveBtns = document.querySelectorAll('.saveBtn')
   saveBtns.forEach(function (saveBtn) {
@@ -19,13 +11,6 @@ $(function () {
     });
   });
 
-
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
   const divs = document.getElementsByTagName('div')
   for (let i = 0; i < divs.length; i++) {
     const idTime = parseInt(divs[i].getAttribute('id'))
@@ -42,13 +27,6 @@ $(function () {
     }
   };
 
-
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-
   for (let i = 0; i < localStorage.length; i++) {
     const key = localStorage.key(i);
     const value = localStorage.getItem(key);
@@ -63,7 +41,6 @@ $(function () {
 
   };
 
-  // TODO: Add code to display the current date in the header of the page.
   function dayTime() {
     const date = dayjs();
     $('#currentDay').text(date.format('MMMM D, YYYY'))
